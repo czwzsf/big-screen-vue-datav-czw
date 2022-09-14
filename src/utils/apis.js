6900a7ec682aa7a896a7c0380bf995123fba2be4
zhewei chen <1241770441@qq.com>
2022-09-14 19:24:04 +0800

    Initial commit

---
+++ src/utils/apis.js
@@ -0,0 +1,31 @@
+/**
+ * 用来存放项目中所有接口的地址
+ */
+const apiHost = 'http://localhost:8080/api'
+/**
+ * 用户账户接口
+ */
+const AccountApis = {
+    loginUrl: '/',
+    logoutUrl: '/'
+}
+/**
+ * 系统模块的接口
+ * @type {{sliderListUrl: string}}
+ */
+const SystemApis = {
+    sliderListUrl: apiHost + '/system/slider/list/'
+}
+
+/**
+ * 图表接口
+ */
+const ChartApis = {
+    bottomLeftUrl: apiHost + '/chart/bottom/bottomLeftChart/',
+    bottomRightUrl: apiHost + '/chart/bottom/bottomrightChart/',
+}
+export {
+    AccountApis,
+    SystemApis,
+    ChartApis
+}
