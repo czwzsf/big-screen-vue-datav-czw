42066ee3f59247ed4d880af6f067671fdc921127
zhewei chen <1241770441@qq.com>
2022-09-15 10:06:03 +0800

    零部件最近月12mis(w)

--- src/views/centerRight1.vue
+++ src/views/centerRight1.vue
@@ -10,7 +10,7 @@
         </div>
       </div>
       <div class="d-flex jc-center body-box">
-        <dv-scroll-board class="dv-scr-board" :config="config" />
+        <dv-scroll-board class="dv-scr-board" :config="config"/>
       </div>
     </div>
   </div>
@@ -21,26 +21,27 @@ export default {
   data() {
     return {
       config: {
-        header: ['组件', '分支', '覆盖率'],
+        header: ['零部件', '当前值', '环比', '比基础值', '同比'],
         data: [
-          ['组件1', 'dev-1', "<span  class='colorGrass'>↑75%</span>"],
-          ['组件2', 'dev-2', "<span  class='colorRed'>↓33%</span>"],
-          ['组件3', 'dev-3', "<span  class='colorGrass'>↑100%</span>"],
-          ['组件4', 'rea-1', "<span  class='colorGrass'>↑94%</span>"],
-          ['组件5', 'rea-2', "<span  class='colorGrass'>↑95%</span>"],
-          ['组件6', 'fix-2', "<span  class='colorGrass'>↑63%</span>"],
-          ['组件7', 'fix-4', "<span  class='colorGrass'>↑84%</span>"],
-          ['组件8', 'fix-7', "<span  class='colorRed'>↓46%</span>"],
-          ['组件9', 'dev-2', "<span  class='colorRed'>↓13%</span>"],
-          ['组件10', 'dev-9', "<span  class='colorGrass'>↑76%</span>"]
+          ['水泵总成', '21.3', '3.5', '9.7', '3.5'],
+          ['制动电磁阀总成', '3.9', '-0.4', '-7', '-0.4'],
+          ['垫密片-排气管', '21.3', '-1.1', '-4.2', '-1.1'],
+          ['垫密片-上气缸盖罩盖', '6.2', '-2.5', '-3.6', '-2.5'],
+          ['平惰轮总成', '10.8', '-0.1', '-4.6', '-0.1'],
+          ['喷油器总成', '12.3', '-1.3', '0.5', '-1.3'],
+          ['减速起动机', '4.5', '0.3', '-2.3', '0.3'],
+          ['垫片-增压器', '4.2', '0.5', '2', '0.5'],
+          ['排气歧管', '4.5', '1.3', '-2.7', '1.3'],
+          ['涡轮增压器', '3.7', '-0.4', '-2.2', '-0.4'],
+          ['进油计量比例阀', '2.4', '-1.2', '-2.3', '-1.2'],
         ],
-        rowNum: 7, //表格行数
+        rowNum: 11, //表格行数
         headerHeight: 35,
-        headerBGC: '#0f1325', //表头
-        oddRowBGC: '#0f1325', //奇数行
-        evenRowBGC: '#171c33', //偶数行
+        headerBGC: '#062a79', //表头
+        oddRowBGC: '#8c0ed5', //奇数行
+        evenRowBGC: '#e1786e', //偶数行
         index: true,
-        columnWidth: [50],
+        columnWidth: [100],
         align: ['center']
       }
     }
@@ -49,27 +50,31 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-$box-height: 410px;
-$box-width: 300px;
+$box-height: 270px;
+$box-width: 600px;
 #centerRight1 {
   padding: 16px;
   padding-top: 20px;
   height: $box-height;
   width: $box-width;
   border-radius: 5px;
+
   .bg-color-black {
     height: $box-height - 30px;
     border-radius: 10px;
   }
+
   .text {
     color: #c3cbde;
   }
+
   .body-box {
     border-radius: 10px;
     overflow: hidden;
+
     .dv-scr-board {
-      width: 270px;
-      height: 340px;
+      width: 600px;
+      height: 350px;
     }
   }
 }
